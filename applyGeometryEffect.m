d9c903905136206171cc523e33e0e28fed4bf624
Ellis <ems236@case.edu>
2020-07-19 02:13:13 -0400

    Added simple geometry

--- applyGeometryEffect.m
+++ applyGeometryEffect.m
@@ -1,21 +1,25 @@
 function outImg=applyGeometryEffect(img, s, theta, m, t)
 
 [rows, cols] = size(img);
-outImg = zeros(rows, cols);
+outImg = uint8(zeros(rows, cols));
 
 %want center of img at 0,0
-toGeometricCoords = [0, 0, -(double(cols) + 1) / 2;
-                       0, 0, (double(rows) + 1) / 2;
+toGeometricCoords = [1, 0, -(double(cols) + 1) / 2;
+                       0, -1, (double(rows) + 1) / 2;
                        0, 0, 1];
-toImgCoords = [0, 0, (double(cols) + 1) / 2;
-                       0, 0, -(double(rows) + 1) / 2;
+toImgCoords = [1, 0, (double(cols) + 1) / 2;
+                       0, -1, (double(rows) + 1) / 2;
                        0, 0, 1];
 
-inverseMat = matrixMult(toImgCoords, matrixMult(inverseCompoundEffect(s, theta, m, t), toGeometricCoords));
+inverseEffect = inverseCompoundEffect(s, theta, m, t);
+inversedGeometric = matrixMult(inverseEffect, toGeometricCoords);
+inverseMat = matrixMult(toImgCoords, inversedGeometric);
+%inverseMat = matrixMult(toImgCoords, matrixMult(inverseCompoundEffect(s, theta, m, t), toGeometricCoords))
 
 for row = 1:rows
     for col = 1:cols
         imgCoords = matrixMult(inverseMat, [col; row; 1]);
-        outImg(row, col) = bilinearInterpolate(img, imgCoords(2), imgCoords(1));
+        pxVal = bilinearInterpolate(img, imgCoords(2), imgCoords(1));
+        outImg(row, col) = pxVal;
     end
 end
