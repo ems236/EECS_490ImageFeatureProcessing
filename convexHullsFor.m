d6b550af6d29f40f8ad36ea1c296f747fb796875
Ellis <ems236@case.edu>
2020-07-22 02:46:17 -0400

    Made parsing left to right

--- convexHullsFor.m
+++ convexHullsFor.m
@@ -5,8 +5,8 @@ function hulls=convexHullsFor(img)
     visited = zeros(maxVal(characters),1);
     
     hullNum = 1;
-    for row = 1:rows
-        for col = 1:cols
+    for col = 1:cols
+        for row = 1:rows
             if characters(row, col) > 1 && visited(characters(row, col)) == 0
                 hulls(hullNum, :) = hullBoundsFor(characters, characters(row, col));
                 hullNum = hullNum + 1;
